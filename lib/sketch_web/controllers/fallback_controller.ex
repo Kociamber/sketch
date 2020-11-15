23274cdd7a88d5adc1966d6f6802e20ae2638f25
Rafal Kociszewski <rafal.kociszewski@gmail.com>
2020-11-15 16:32:00 +0100

    remove obsolete controller and add linking

--- lib/sketch_web/controllers/fallback_controller.ex
+++ lib/sketch_web/controllers/fallback_controller.ex
@@ -6,14 +6,6 @@ defmodule SketchWeb.FallbackController do
   """
   use SketchWeb, :controller
 
-  # This clause handles errors returned by Ecto's insert/update/delete.
-  # def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
-  #   conn
-  #   |> put_status(:unprocessable_entity)
-  #   |> put_view(SketchWeb.ChangesetView)
-  #   |> render("error.json", changeset: changeset)
-  # end
-
   # This clause is an example of how to handle resources that cannot be found.
   def call(conn, {:error, :not_found}) do
     conn
--- lib/sketch_web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule SketchWeb.PageController do
-  use SketchWeb, :controller
-
-  def index(conn, _params) do
-    render(conn, "index.html")
-  end
-end
